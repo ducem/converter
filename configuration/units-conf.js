1ec223772a36e6227518d6edab8171f252687e5d
cedric <cedric.raudin@gmail.com>
2013-12-21 09:09:23 -0800

    add volume-conf

--- configuration/units-conf.js
+++ configuration/units-conf.js
@@ -3,7 +3,8 @@ exports.Conf = {
     UnitsConf: {
         TemperatureUnitsConf: require("configuration/temperature-units-conf").TemperatureUnitsConf,
         WeightUnitsConf: require("configuration/weight-units-conf").WeightUnitsConf,
-        AreaUnitsConf: require("configuration/area-units-conf").AreaUnitsConf
+        AreaUnitsConf: require("configuration/area-units-conf").AreaUnitsConf,
+        VolumeUnitsConf: require("configuration/volume-units-conf").VolumeUnitsConf
     },
 
     MetricPrefix: require("configuration/metric-prefix").MetricPrefix
